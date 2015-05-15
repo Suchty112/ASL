5379306e5b8fadd05ff075fb5daf9080739769db
Fabian H <fabianhassels@hotmail.de>
2015-05-15 18:25:47 +0200

    URL auf Github umgestellt

--- script.user.js
+++ script.user.js
@@ -13,5 +13,5 @@ document.body.appendChild(scriptElement);
 
 var scriptElement = document.createElement( "script" );
 scriptElement.type = "text/javascript";
-scriptElement.src = "http://test/script.dev.js";
+scriptElement.src = "https://raw.githubusercontent.com/eaglefsd/ASL/master/script.dev.js";
 document.body.appendChild(scriptElement);
