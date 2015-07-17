a21cdf2960a80e1b62262de85036bd51ee88265e
Fabian H. <fabianhassels@hotmail.de>
2015-07-17 18:13:14 +0200

    jQuery Cookie komplett entfernt
    Wird nicht mehr benötigt, da die eingeklappten Wachen nicht mehr funktionieren und Sebastian sowieso jQuery Cookie eingebunden hat

--- script.user.js
+++ script.user.js
@@ -11,8 +11,3 @@ var scriptElement = document.createElement( "script" );
 scriptElement.type = "text/javascript";
 scriptElement.src = "https://rawgit.com/carhartl/jquery-cookie/master/src/jquery.cookie.js";
 document.body.appendChild(scriptElement);
-
-var scriptElement = document.createElement( "script" );
-scriptElement.type = "text/javascript";
-scriptElement.src = "https://rawgit.com/eaglefsd/ASL/master/script.dev.js";
-document.body.appendChild(scriptElement);
