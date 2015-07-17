b9281bf08e741ed449a8ff2bc89a293097083880
Fabian H. <fabianhassels@hotmail.de>
2015-07-17 18:40:20 +0200

    Richtige Version wiederhergestellt :D

--- script.user.js
+++ script.user.js
@@ -9,5 +9,5 @@
 
 var scriptElement = document.createElement( "script" );
 scriptElement.type = "text/javascript";
-scriptElement.src = "https://rawgit.com/carhartl/jquery-cookie/master/src/jquery.cookie.js";
+scriptElement.src = "https://rawgit.com/eaglefsd/ASL/master/script.dev.js";
 document.body.appendChild(scriptElement);
