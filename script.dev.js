f3ca80daa450b0c89df2b6b114fe2ccd1d02b197
Fabian H <fabianhassels@hotmail.de>
2016-10-13 20:01:10 +0200

    FZG-Button jetzt unten

--- script.dev.js
+++ script.dev.js
@@ -430,7 +430,7 @@ function countPatients() {
 // Fahrzeugtypen statt Name bei Klick auf Button
 function showCarTypesInsteadOfStation() {
     // Button neben dem ersten "Alarmieren"-Button erstellen
-    $('#missionH1').after('<button type="button" id="scriptShowCarTypes" class="btn btn-info btn-mini">' +
+    $('#mission_finish_now_btn').after('<button type="button" id="scriptShowCarTypes" class="btn btn-info btn-sm">' +
         'Fzg.-Typen anzeigen</button>');
     $('#scriptShowCarTypes').bind('click', function () {
             $('td[vehicle_type_id]').each(function () {
@@ -514,9 +514,15 @@ function fayeEvent() {
     }
 }
 
+// Piwik für Nutzungsstatistiken
+function piwik() {
+    $('body').append('<img src="//piwik.eagledev.de/piwik.php?idsite=1" style="border:0;" alt="" />');
+}
+
 // Fallunterscheidung für die verschiedenen Seiten
 if (window.location.pathname === '/') {
     // Startseite
+    piwik();
     if (settings.missionTabs) {
         tabsForMissions();
     }
--- script.user.js
+++ script.user.js
@@ -9,5 +9,5 @@
 
 var scriptElement = document.createElement("script");
 scriptElement.type = "text/javascript";
-scriptElement.src = "https://rawgit.com/eaglefsd/ASL/master/script.dev.js";
+scriptElement.src = "https://lss.hassels.eu/ASL/script.dev.js";
 document.body.appendChild(scriptElement);
